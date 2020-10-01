bc81b7ce637c2355136b03bdb3d3aeee21e91730
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-01 20:38:20 -0300

    Added the useMemo hook for define the react components

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Header from '../../components/Header'
+import { View } from 'react-native'
 
 import {
   Wrapper,
@@ -7,7 +8,55 @@ import {
   Main
 } from './styles'
 
+interface Item {
+  key: string,
+  render: () => JSX.Element,
+  isTitle?: boolean
+}
+
 const Following: React.FC = () => {
+  useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <View />
+      },
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C1', render: () => <View />, },
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C2', render: () => <View />, },
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C3', render: () => <View />, },
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: 'C4', render: () => <View />, },
+    ]
+
+    const indexes: number[] = []
+
+    items.forEach((item, index) => item.isTitle && indexes.push(index))
+
+    return {
+      data: items,
+      indexes
+    }
+  }, [])
+
   return (
     <Wrapper>
       <Container>
